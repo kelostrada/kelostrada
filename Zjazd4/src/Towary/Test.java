9b98690b448255507ab460c744175680e6221433
kelostrada@gmail.com <kelostrada@gmail.com@da480675-f5f3-04c8-a99f-176209404a79>
2013-05-08 13:44:25 +0000

    FileGenerator Gotowy

--- Zjazd4/src/Towary/Test.java
+++ Zjazd4/src/Towary/Test.java
@@ -6,6 +6,8 @@ package Towary;
 
 public class Test {
     public static void main(String[] args) {
+        //FileGenerator.GenerateFile("C:\\Temp\\Towary.txt", 1000000);
+        
         
     }
 }
