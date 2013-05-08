567152a600b82882d222469a123cc947470c47cc
kelostrada@gmail.com <kelostrada@gmail.com@da480675-f5f3-04c8-a99f-176209404a79>
2013-05-08 20:55:34 +0000

    Przeglądarka skończona (chyba działa dobrze :P ale kod brzydki jak cholera)

--- Zjazd4/src/PrzegladarkaObrazkow/Przegladarka.java
+++ Zjazd4/src/PrzegladarkaObrazkow/Przegladarka.java
@@ -4,9 +4,9 @@ package PrzegladarkaObrazkow;
  * @author Kelu
  */
 import java.awt.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.*;
+import java.io.File;
+import java.io.FilenameFilter;
 
 class ImagePanel extends JPanel {
 
@@ -27,8 +27,7 @@ class ImagePanel extends JPanel {
         }
     }
 
-    public void loadImage(String imgFileName) {
-        this.repaint();
+    void loadImage(String imgFileName) {
         img = Toolkit.getDefaultToolkit().getImage(imgFileName);
         MediaTracker mt = new MediaTracker(this);
         mt.addImage(img, 1);
@@ -39,9 +38,54 @@ class ImagePanel extends JPanel {
         int w = img.getWidth(this); // szerokość obrazka
         int h = img.getHeight(this); // wysokość obrazka
 
-        loaded = true;
-        setPreferredSize(new Dimension(w, h));
+        if (w != -1 && w != 0 && h != -1 && h != 0) {
+            loaded = true;
+            setPreferredSize(new Dimension(w, h));
+        } else {
+            setPreferredSize(new Dimension(300, 200));
+            loaded = false;
+        }
 
+    }
+}
+
+class FinalPanel extends JPanel {
+
+    private int size;
+
+    public FinalPanel(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        setPreferredSize(new Dimension(size * 13, size * 13));
+        g.setColor(Color.RED);
+        g.setFont(new Font("Arial", Font.PLAIN, size));
+        drawBorders(getWidth(), getHeight(), g);
+        drawCenteredString("Koniec prezentacji", getWidth(), getHeight(), g);
+    }
+
+    private void drawCenteredString(String s, int w, int h, Graphics g) {
+        FontMetrics fm = g.getFontMetrics();
+        int x = (w - fm.stringWidth(s)) / 2;
+        int y = (fm.getAscent() + (h - (fm.getAscent() + fm.getDescent())) / 2);
+        g.drawString(s, x, y);
+    }
+
+    private void drawBorders(int w, int h, Graphics g) {
+        int x = 10;
+        int y = 15;
+        g.drawLine(x, x, x, h - x);
+        g.drawLine(x, x, w - x, x);
+        g.drawLine(w - x, x, w - x, h - x);
+        g.drawLine(x, h - x, w - x, h - x);
+        g.drawLine(y, y, y, h - y);
+        g.drawLine(y, y, w - y, y);
+        g.drawLine(w - y, y, w - y, h - y);
+        g.drawLine(y, h - y, w - y, h - y);
+        g.drawLine(y, y, w - y, h-y);
     }
 }
 
@@ -49,12 +93,17 @@ public class Przegladarka extends JFrame {
 
     ImagePanel p;
 
+    public Przegladarka() {
+        this("");
+    }
+
     public Przegladarka(String fname) {
         p = new ImagePanel(fname);
         add(p);
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public void loadImage(String imgFileName) {
@@ -63,41 +112,64 @@ public class Przegladarka extends JFrame {
         setLocationRelativeTo(null);
     }
 
+    public void loadFinalPanel(int size) {
+
+        remove(p);
+        FinalPanel f = new FinalPanel(size);
+        add(f);
+        f.paintComponent(getGraphics());
+
+        pack();
+    }
+    static final String[] EXTENSIONS = new String[]{
+        "gif", "jpg"
+    };
+    static final FilenameFilter IMAGE_FILTER = new FilenameFilter() {
+        @Override
+        public boolean accept(final File dir, final String name) {
+            for (final String ext : EXTENSIONS) {
+                if (name.endsWith("." + ext)) {
+                    return (true);
+                }
+            }
+            return (false);
+        }
+    };
+
     public static void main(String args[]) {
-        
-        String temp = JOptionPane.showInputDialog(null,"Podaj katalog z obrazkami\nczas wyświetlania jednego obrazka (w sek.)\nrozmiar pisma dla napisu 'Koniec Prezentacji'","Input", JOptionPane.QUESTION_MESSAGE);
+
+        String temp = JOptionPane.showInputDialog(null, "Podaj katalog z obrazkami\nczas wyświetlania jednego obrazka (w sek.)\nrozmiar pisma dla napisu 'Koniec Prezentacji'", "Input", JOptionPane.QUESTION_MESSAGE);
         String[] parametry = null;
         if (temp == null || (parametry = temp.split(" ")).length != 3) {
             System.err.println("Błędna ilość parametrów");
             System.exit(-1);
         }
-        
-        String path = parametry[0];
+
+        File path = new File(parametry[0]);
+        int time = 0;
+        int size = 0;
         try {
-        int time = Integer.parseInt(parametry[1]);
-        int size = Integer.parseInt(parametry[2]);
+            time = Integer.parseInt(parametry[1]);
+            size = Integer.parseInt(parametry[2]);
         } catch (NumberFormatException ex) {
             System.err.println("Niepoprawne parametry.");
             System.exit(-2);
         }
-        
-        
-        Przegladarka p = new Przegladarka("beach.jpg");
 
-        for (int i = 0; i < 10; i++) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Przegladarka.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            p.loadImage("temp.jpg");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Przegladarka.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            p.loadImage("beach.jpg");
+        Przegladarka p = new Przegladarka();
+        p.setTitle("Przeglądarka Obrazków");
 
+        if (path.isDirectory()) {
+            for (final File f : path.listFiles(IMAGE_FILTER)) {
+                p.loadImage(f.getPath());
+                try {
+                    Thread.sleep(time * 1000);
+                } catch (InterruptedException ex) {
+                }
+            }
         }
+
+        p.loadFinalPanel(size);
+
     }
 }
