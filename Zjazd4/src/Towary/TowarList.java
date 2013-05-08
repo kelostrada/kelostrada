733117fa5c2228c3167c6f4836c5ea2ee0c60046
kelostrada@gmail.com <kelostrada@gmail.com@da480675-f5f3-04c8-a99f-176209404a79>
2013-05-08 15:14:14 +0000

    Lista poprawiona (wyszukiwanie elementu od tyłu dodane dla szybkości działania). Nadal nie idealna lista, ale zawsze coś

--- Zjazd4/src/Towary/TowarList.java
+++ Zjazd4/src/Towary/TowarList.java
@@ -3,9 +3,71 @@ package Towary;
 /**
  * @author bartosz.kalinowski
  */
-
 public class TowarList {
+
     TowarNode head;
-    
-    
+    TowarNode tail;
+    private int size;
+
+    TowarList() {
+        head = tail = null;
+        size = 0;
+    }
+
+    TowarList(TowarNode t) {
+        head = tail = t;
+        size = 1;
+    }
+
+    void add(Towar t) {
+        TowarNode node = new TowarNode(t);
+        if (head == null) {
+            head = tail = node;
+        } else if (head == tail) {
+            tail = node;
+            head.next = tail;
+            tail.prev = head;
+        } else {
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+        }
+        size++;
+    }
+
+    Towar get(int i) {
+        TowarNode node = head;
+        if (node == null) {
+            return null;
+        }
+
+        if (i >= size) {
+            return null;
+        }
+
+        if (size - i >= i) {
+            int counter = 0;
+            while (counter < i) {
+                node = node.next;
+                counter++;
+            }
+        } else {
+            node = tail;
+            int counter = size-1;
+            while (counter > i) {
+                node = node.prev;
+                counter--;
+            }
+        }
+
+        if (node != null) {
+            return node.towar;
+        } else {
+            return null;
+        }
+    }
+
+    int size() {
+        return size;
+    }
 }
